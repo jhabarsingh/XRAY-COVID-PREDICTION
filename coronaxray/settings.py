3ab8bf29d3849b7d907cd4579b1c042a82c7b9d3
jhabarsingh <jhabarsinghbhati23@gmail.com>
2020-08-03 00:50:10 +0530

    created api for corona xray report

--- coronaxray/settings.py
+++ coronaxray/settings.py
@@ -77,12 +77,7 @@ WSGI_APPLICATION = 'coronaxray.wsgi.application'
    
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3'
     }
 }
 
