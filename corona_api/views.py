d2a6d6427296148fc4285a897185d81fe062bdcc
jhabarsingh <43932986+jhabarsingh@users.noreply.github.com>
2021-01-17 10:24:48 +0530

    refactored views.py code

--- corona_api/views.py
+++ corona_api/views.py
@@ -11,19 +11,9 @@ def get_corona(request, *args, **kwargs):
 	'''Takes xray report as an input and preedict weather the patient is corona positive or not
 	'''
 	if request.method == 'POST':
-<<<<<<< HEAD
-		print(request.FILES)
-		# data = request.POST
-		# image = request.FILES['image']
-		# print(request.FILES)
-		# image = Image.open(image).convert('RGB')
-		# data = xray_production.predict(image)
-		# return JsonResponse({'data': data})	
-=======
 		data = request.POST
 		image = request.FILES['image']
 		image = Image.open(image).convert('RGB')
 		data = xray_production.predict(image)
 		return JsonResponse({'data': data})	
->>>>>>> 7e4ce46111d38050ec065519123b5c2a2fddfbd3
 	return JsonResponse({'msg':'GET'})
