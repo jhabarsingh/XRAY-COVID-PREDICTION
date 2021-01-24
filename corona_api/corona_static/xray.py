24746c0c23840b75358d0f0fec3d96739a049e08
jhabarsingh <43932986+jhabarsingh@users.noreply.github.com>
2021-01-25 00:29:35 +0530

    ML Model doc string changed

--- corona_api/corona_static/xray.py
+++ corona_api/corona_static/xray.py
@@ -2,10 +2,6 @@
 Detecting COVID-19 with Chest X Ray using PyTorch
 Image classification of Chest X Rays in one of three classes: Normal, Viral Pneumonia, COVID-19
 
-Notebook created for the guided project Detecting COVID-19 with Chest X Ray using PyTorch on Coursera
-
-Dataset from COVID-19 Radiography Dataset on Kaggle
-
 Importing Libraries
 '''
 
